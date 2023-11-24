7ebc3b191646a8a41091f46fb265684011afcd15
Eric Schwalb <ericpossollo@gmail.com>
2023-11-24 21:19:42 +0000

    configs de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/reactthreejs01',
   plugins: [react()],
+  base: '/reactthreejs01',
   build: {
     rollupOptions: {
     },
