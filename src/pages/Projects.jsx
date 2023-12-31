4f4651a17537e343435d2d19704a20e5e9f27f3b
Eric Schwalb <ericpossollo@gmail.com>
2023-12-31 16:39:01 +0000

    start: products page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel"
 
 const Projects = () => {
   return (
     <section className='w-full h-screen text-white font-bold bg-stone-900 relative'>
+      <Carousel>
+  <CarouselContent>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
       </section>
   )
 }
